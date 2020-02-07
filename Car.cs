180e413a83c40ad8f252510b0211a286200c8cc3
vi-hseng <46452844+vi-hseng@users.noreply.github.com>
2020-02-07 12:21:28 -0500

    Intial Commit
    Files that were edited in class

---
+++ Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Car
+    {
+        public string color;
+        public string model;
+        public int maxSpeed;
+        public int year;
+
+        public void fullThrottle()
+        {
+            Console.WriteLine("The car is going as fast as it can!");
+        }
+
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            Car myObj1 = new Car();
+
+            myObj.color = "red";
+            myObj.maxSpeed = 200;
+
+            Console.WriteLine(myObj.color);
+            Console.WriteLine(myObj1.color);
+            Console.WriteLine(myObj.maxSpeed);
+            myObj.fullThrottle();
+        }        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car Ford = new Car();
+            Car Opel = new Car();
+            Ford.model = "Mustang";
+            Opel.model = "Astra";
+            Ford.color = "red";
+            Opel.color = "white";
+            Ford.year = 1969;
+            Opel.year = 2005;
+
+            Console.WriteLine(Ford.model);
+            Console.WriteLine(Opel.model);
+        }
+    }
+}
